daf20576829616020e1ebb3b4709ceb2ef61f601
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-07 21:02:18 +0100

    creazione view comics.show

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title')
+    {{ $comic['title'] }}
+@endsection
+
+@section('content')
+    <div class="img-container text-center">
+        <img src="{{ asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
+    </div>
+
+    <div class="comic-info p-2">
+        <h1> {{ $comic['title'] }} </h1>
+
+        <p> {{ $comic['description'] }} </p>
+
+        <p> {{ $comic['price'] }} </p>
+    </div>
+
+    <div class="btn btn-primary">
+        <a class="text-white text-decoration-none" href="{{ route('comic.index') }}">Back</a>
+    </div>
+@endsection
