11f144cb06fcfbd1eb303ba0b87f0bf7ce2ce94b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-07 20:21:49 +0100

    riempimento tabella dc_comics con data.comics

---
+++ database/seeds/ComicTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\models\Comic;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $comics = config('data.comics');
+        foreach ($comics as $model) {
+            $comic = new Comic();
+            $comic['title'] = $model['title'];
+            $comic['description'] = $model['description'];
+            $comic['thumb'] = $model['thumb'];
+            $comic['price'] = $model['price'];
+            $comic['series'] = $model['series'];
+            $comic['sale_date'] = $model['sale_date'];
+            $comic['type'] = $model['type'];
+
+            $comic->save();
+        }
+    }
+}
