1dbb8ce2e6063891e4d359f886779621de5b9374
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-14 11:55:56 +0100

    aggiunta validate in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,6 +5,16 @@
 @endsection
 
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form method="POST" action="{{ route('comic.update', $comic->id) }}">
         @csrf
         @method('PUT')
@@ -12,31 +22,31 @@
 
         {{-- Titolo fumetto --}}
         <label for="title">Titolo: </label>
-        <input type="text" name="title" id="title" value="{{ $comic['title'] }}">
+        <input type="text" name="title" id="title" value="{{ old('title', $comic['title']) }}">
 
         {{-- Descrizione fumetto --}}
         <label for="description">Descrizione: </label>
-        <input type="text" name="description" id="description" value="{{ $comic['description'] }}">
+        <input type="text" name="description" id="description" value="{{ old('description', $comic['description']) }}">
 
         {{-- Link thumb --}}
         <label for="thumb">Link immagine: </label>
-        <input type="url" name="thumb" id="thumb" value="{{ $comic['thumb'] }}">
+        <input type="url" name="thumb" id="thumb" value="{{ old('thumb', $comic['thumb']) }}">
 
         {{-- Prezzo fumetto --}}
         <label for="price">Prezzo: </label>
-        <input type="number" step=".01" name="price" id="price" value="{{ $comic['price'] }}">
+        <input type="number" step=".01" name="price" id="price" value="{{ old('price', $comic['price']) }}">
 
         {{-- Serie del fumetto --}}
         <label for="series">Serie: </label>
-        <input type="text" name="series" id="series" value="{{ $comic['series'] }}">
+        <input type="text" name="series" id="series" value="{{ old('series', $comic['series']) }}">
 
         {{-- Data di inizio vendita --}}
         <label for="sale_date">Data di vendita: </label>
-        <input type="date" name="sale_date" id="sale_date" value="{{ $comic['sale_date'] }}">
+        <input type="date" name="sale_date" id="sale_date" value="{{ old('sale_date', $comic['sale_date']) }}">
 
         {{-- Tipo fumetto --}}
         <label for="type">Tipo: </label>
-        <input type="text" name="type" id="type" value="{{ $comic['type'] }}">
+        <input type="text" name="type" id="type" value="{{ old('type', $comic['type']) }}">
 
         <input type="submit" value="Aggiorna">
 
